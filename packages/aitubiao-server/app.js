0278a824014f87134b14ef69b508f4c5580940ca
uikoo9 <uikoo9@qq.com>
2025-12-03 11:16:34 +0800

    feat(@shun-js/aitubiao-server): app.js

--- packages/aitubiao-server/app.js
+++ packages/aitubiao-server/app.js
@@ -1,35 +1,42 @@
 // config
-const config = require('./server/config.js');
-
-// sentry
-const options = {};
-
-// options cros
-options.cros = true;
-
-// options config
-options.config = config;
-
-// options.redis
-options.redis = require('qiao-redis');
-options.redisOptions = config.redisOptions;
-
-// options log
-options.log = require('qiao-log');
-options.logOptions = require('./server/log-options.js')();
-
-// options rate limit
-options.rateLimitLib = require('qiao-rate-limit');
-options.rateLimitOptions = config.rateLimitOptions;
-
-// options checks
-options.checks = [require('./server/util/check.js').checkUserAuth];
-
-// options modules
-options.modules = [require('qiao-z-sms').init, require('qiao-z-nuser').init];
+const { parseServerConfig } = require('@shun-js/shun-config');
 
 // init
 (async () => {
+  // config
+  const config = await parseServerConfig(process.argv);
+  if (!config) {
+    console.log('read server config fail');
+    return;
+  }
+
+  // options
+  const options = {};
+
+  // options cros
+  options.cros = true;
+
+  // options config
+  options.config = config;
+
+  // options.redis
+  options.redis = require('qiao-redis');
+  options.redisOptions = config.redisOptions;
+
+  // options log
+  options.log = require('qiao-log');
+  options.logOptions = require('./server/log-options.js')();
+
+  // options rate limit
+  options.rateLimitLib = require('qiao-rate-limit');
+  options.rateLimitOptions = config.rateLimitOptions;
+
+  // options checks
+  options.checks = [require('./server/util/check.js').checkUserAuth];
+
+  // options modules
+  options.modules = [require('qiao-z-sms').init, require('qiao-z-nuser').init];
+
   const app = await require('qiao-z')(options);
-  app.listen(8002);
+  app.listen(config.port);
 })();
