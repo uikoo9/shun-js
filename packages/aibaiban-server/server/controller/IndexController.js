08cfb5ec2d0b84b94c230bed9c4a2631499d4791
uikoo9 <uikoo9@qq.com>
2026-01-26 19:35:28 +0800

    feat(@shun-js/aibaiban-server): landing ok

--- packages/aibaiban-server/server/controller/IndexController.js
+++ packages/aibaiban-server/server/controller/IndexController.js
@@ -9,70 +9,9 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     service.index(req, res);
   });
-  app.get('/landing-preview.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-preview.html';
 
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
-  });
-  app.get('/landing-v1.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-v1.html';
-
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
-  });
-  app.get('/landing-v2.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-v2.html';
-
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
-  });
-  app.get('/landing-v3.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-v3.html';
-
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
-  });
-  app.get('/landing-v4.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-v4.html';
-
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
-  });
-  app.get('/landing-v5.html', async (req, res) => {
-    // const
-    const pagePath = './views/landing-v5.html';
-
-    // is static
-    const isStatic = await res.staticRender(pagePath);
-    if (isStatic) return;
-
-    // render
-    res.render(pagePath, {}, true);
+  // board
+  app.get('/board', (req, res) => {
+    service.board(req, res);
   });
 };
--- packages/aibaiban-server/server/service/IndexService.js
+++ packages/aibaiban-server/server/service/IndexService.js
@@ -14,3 +14,20 @@ exports.index = async (req, res) => {
   // render
   res.render(pagePath, {}, true);
 };
+
+/**
+ * board
+ * @param {*} req
+ * @param {*} res
+ */
+exports.board = async (req, res) => {
+  // const
+  const pagePath = './views/board.html';
+
+  // is static
+  const isStatic = await res.staticRender(pagePath);
+  if (isStatic) return;
+
+  // render
+  res.render(pagePath, {}, true);
+};
